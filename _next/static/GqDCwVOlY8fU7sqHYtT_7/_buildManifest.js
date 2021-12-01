86caad8a537bedf21a86fbc5b1695c529a376bb5
TVQuizPhD <tvquizphd@gmail.com>
2021-12-01 05:55:30 +0000

    initial

---
+++ _next/static/GqDCwVOlY8fU7sqHYtT_7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/[[...slug]]":["static/chunks/80-c6433cdd20612f18.js","static/css/acd23987a3ae8842.css","static/chunks/pages/[[...slug]]-8cc00acd358aacd7.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
